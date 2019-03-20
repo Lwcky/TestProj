9ac528de44b40aaf8c21ce773de86965a730e4b6
kunyuan liu <kunyuanliu@kunyuans-MacBook-Pro.local>
2019-03-20 16:28:27 +0800

    create

---
+++ TestJenkinsProj/TestJenkinsProj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestJenkinsProj
+//
+//  Created by kunyuan liu on 2019/3/20.
+//  Copyright © 2019 kunyuan liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestJenkinsProj/TestJenkinsProj/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestJenkinsProj
+//
+//  Created by kunyuan liu on 2019/3/20.
+//  Copyright © 2019 kunyuan liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
